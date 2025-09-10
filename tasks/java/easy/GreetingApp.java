3db14779af2f4e19e435c9f4b8cb4df9f611dcdf
Loukas Mastorekas <lmastorek@csd.auth.gr>
2025-09-10 21:44:24 +0300

    building the Greeting App 1.1

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class GreetingApp {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
